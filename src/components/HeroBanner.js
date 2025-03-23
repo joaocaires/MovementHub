ec05aeabf6dcd2f72a0f0e4b6a2d4c4d05b844fc
Joao Caires <jprcaires@gmail.com>
2025-03-23 17:48:01 -0300

    project redesign

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,31 +1,35 @@
-import React from "react";
-import { Box, Stack, Typography, Button } from '@mui/material'
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
-import Banner from '../assets/images/banner.jpg'
+import HeroBannerImage from '../assets/images/banner.jpg';
 
-const HeroBanner = () => {
-    return(
-        <Box sx={{ mt: {lg: '190px', xs: '70px'}, ml: { sm: '50px' } }} position="relative" p="20px">
-            <Typography color="#FF2625" fontWeight="600" fontSize="26px">
-                Fitness Club
-            </Typography>
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '180px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
 
-            <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' }
-            }} mb="23px" mt="30px"> Sweat, Smile <br/> and Repeat </Typography>
+    <Typography color="#2E4053" fontWeight="600" fontSize="35px">Fitness Club</Typography>
 
-            <Typography fontSize="22px" lineHeight="35px" mb={4}> Check out the most effective exercises </Typography>
-            
-            <Button variant="contained" color="success" href="#exercises" sx={{padding: '10px'}}>
-                Explore Exercises
-            </Button>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+      Sweat, Smile <br />
+      And Repeat
+    </Typography>
 
-            <Typography fontWeight={600} color="#ff2625" sx={{ opacity: 0.1, display: { lg: 'block', xs: 'none' }}}
-            fontSize={200}> Exercises </Typography>
+    <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
+      Check out the most effective exercises personalized to you
+    </Typography>
 
-            <img src={Banner} alt="banner" className="hero-banner-img" style={{height: '700px',
-                borderRadius: '50px'}}/>
-        </Box>
-    )
-}
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', 
+        textAlign: 'center', background: '#1E8449', padding: '14px', fontSize: '22px', textTransform: 'none', 
+        color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
 
-export default HeroBanner
+    <Typography fontWeight={600} color="#FDFEFE" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, 
+    fontSize: '200px' }}>
+      Exercise
+    </Typography>
+
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+  </Box>
+);
+
+export default HeroBanner;
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,15 +1,22 @@
 export const exerciseOptions = {
 	method: 'GET',
-	hostname: 'exercisedb.p.rapidapi.com',
 	headers: {
-		'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
-		'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
-	}
-};
-
-export const fetchData = async (url, options) => {
-    const response = await fetch(url, options)
-    const data = await response.json()
-
-    return data
-}
+	  'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+	  'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+	},
+  };
+  
+  export const youtubeOptions = {
+	method: 'GET',
+	headers: {
+	  'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+	  'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+	},
+  };
+  
+  export const fetchData = async (url, options) => {
+	const res = await fetch(url, options);
+	const data = await res.json();
+  
+	return data;
+  };
